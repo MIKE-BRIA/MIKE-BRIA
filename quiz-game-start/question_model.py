642dc4564c1001e8baa1f45fcfbcaa24d8132122
Brian Michael <brianmichaeladero@gmail.com>
2024-02-17 23:55:27 +0300

    quiz game

---
+++ quiz-game-start/question_model.py
@@ -0,0 +1,9 @@
+class Question:
+    
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+        
+        
+new_q = Question("Am I going to make it", "True")
+# print(new_q.text)
