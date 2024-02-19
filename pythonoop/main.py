f7f70d59ad690f41ceee8268923de22c1e681b7a
Brian Michael <brianmichaeladero@gmail.com>
2024-02-19 21:30:37 +0300

    python

--- pythonoop/main.py
+++ pythonoop/main.py
@@ -1,7 +1,22 @@
 #learning about class
 
 class User:
-    pass
+    
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+        
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
-user_1 = User()
+user_1 = User("001", "Michael")
+user_2 = User("002","John")
+print(user_1.id)
+print(user_1.username)
+user_1.follow(user_2)
+print(user_2.followers)
+print(user_2.following)
