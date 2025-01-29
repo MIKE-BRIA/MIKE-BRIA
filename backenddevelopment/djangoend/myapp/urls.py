ba710226ac65d81febef2f3ce3d21e6100e26298
Brian Michael <brianmichaeladero@gmail.com>
2025-01-29 20:14:37 +0300

    backendwithpython

--- backenddevelopment/djangoend/myapp/urls.py
+++ backenddevelopment/djangoend/myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('visitors',views.visitors,name='visitors'),
+    path('counter',views.counter,name='counter')
 ]
--- backenddevelopment/djangoend/myapp/views.py
+++ backenddevelopment/djangoend/myapp/views.py
@@ -3,5 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    name = 'Michael'
-    return render(request,'index.html',{'name':name})
+    context = {
+        'name':'Patrick',
+        'age':34,
+        'nationality':'Kenyan'
+    }
+    return render(request,'index.html',  context)
+
+
+def visitors(request):
+    return render(request,'visitors.html')
+
+
+def counter(request):
+    text=request.GET['text']
+    amount_of_words=len(text.split())
+    return render(request,'counter.html',{'words':amount_of_words})
