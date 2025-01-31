97c61dab5078bd45cc9714f3ba3178f7faab4981
Brian Michael <brianmichaeladero@gmail.com>
2025-01-31 23:41:50 +0300

    backendwithpython

--- backenddevelopment/djangoend/myapp/views.py
+++ backenddevelopment/djangoend/myapp/views.py
@@ -4,22 +4,8 @@ from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name='Everywhere'
-    feature1.details='Our service is very quick and reliable throughout the country and even worldwide'
+    features = Feature.objects.all()
     
-    feature2 = Feature()
-    feature2.id=1
-    feature2.name = 'Good Quality'
-    feature2.details = 'We Make sure the quality of your products arrive in their destination without any interference'
-    
-    feature3 = Feature()
-    feature3.id=2
-    feature3.name='Good Customer Care'
-    feature3.details='We respond to our customers need quickly and we have the best rated customer care service within the industry'
-    
-    features = [feature1,feature2,feature3]
     return render(request,'index.html', {'features':features} )
 
 
