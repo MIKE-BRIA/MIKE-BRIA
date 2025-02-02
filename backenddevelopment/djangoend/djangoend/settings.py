f1ce0f128c03b2874ab4497ef97ba88434cf214f
Brian Michael <brianmichaeladero@gmail.com>
2025-02-02 21:07:56 +0300

    'backend'

--- backenddevelopment/djangoend/djangoend/settings.py
+++ backenddevelopment/djangoend/djangoend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'postgres',
+        'PASSWORD': '115371',
+        'HOST': 'localhost',
+         'PORT': '5432', 
     }
 }
 
